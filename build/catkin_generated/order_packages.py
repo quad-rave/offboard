abf9bb7ff6344e643998a0bfc687b5077e24e88d
bargoss <yesiltepebaran98@gmail.com>
2020-11-13 23:41:07 +0300

    This branch should not be merged

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = '/home/bargos/offboard/src'
 whitelisted_packages = ''.split(';') if '' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/bargos/offboard/devel;/opt/ros/melodic'.split(';') if '/home/bargos/offboard/devel;/opt/ros/melodic' != '' else []
+underlay_workspaces = '/home/bargos/catkin_ws/devel;/home/bargos/offboard/devel;/opt/ros/melodic'.split(';') if '/home/bargos/catkin_ws/devel;/home/bargos/offboard/devel;/opt/ros/melodic' != '' else []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,5 +7,6 @@
 # new environment variables
 
 # modified environment variables
-export PATH='/opt/ros/melodic/bin:/home/bargos/anaconda3/bin:/home/bargos/anaconda3/condabin:/opt/gcc-arm-none-eabi-9-2020-q2-update/bin:/home/bargos/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PATH='/home/bargos/catkin_ws/devel/bin:/home/bargos/offboard/devel/bin:/opt/ros/melodic/bin:/home/bargos/anaconda3/bin:/home/bargos/anaconda3/condabin:/opt/gcc-arm-none-eabi-9-2020-q2-update/bin:/home/bargos/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
 export PWD='/home/bargos/offboard/build'
+export PYTHONPATH='/home/bargos/catkin_ws/devel/lib/python2.7/dist-packages:/home/bargos/offboard/devel/lib/python2.7/dist-packages:/opt/ros/melodic/lib/python2.7/dist-packages:/home/bargos/catkin_ws/devel/lib/python3/dist-packages'
