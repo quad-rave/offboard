bugfix about rotation

src/swarm/src/Swarm/networked_info.py
@@ -43,7 +43,7 @@ class NetworkedInfo(object):
 class VectorInfo(NetworkedInfo): 
     def __init__(self, topic):
         super(VectorInfo, self).__init__(topic)
-        self.value = np.array([0,0,0])
+        self.value = np.array([0.0,0.0,0.0])
 
     def serialize_into_buffer(self, buffer):
         buffer.write_vector3(self.value)

src/swarm/src/Swarm/offboard_py_node.py
@@ -40,6 +40,7 @@ def hmm(topic):
     print("hmmmmmmmm: " + topic.data)
 def start_missions():
     rospy.init_node('destroyer')
+    np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
     rate = rospy.Rate(10)
 
     # define the UAV objects
@@ -108,13 +109,13 @@ def start_missions():
     while True:
         formation_targetpos.value = vec(0.0,0.0,15.0)
         formation_targetpos.publish_data()
-        time.sleep(6)
+        time.sleep(10)
 
         formation_targetrot.axis = 'Z'
-        formation_targetrot.duration = 10
-        formation_targetrot.angle = math.pi * 100
+        formation_targetrot.duration = 20
+        formation_targetrot.angle = 2* math.pi
         formation_targetrot.publish_data()
-        time.sleep(12)
+        time.sleep(120)
 
         formation_targetpos.value = vec(20.0,0.0,15.0)
         formation_targetpos.publish_data()

src/swarm/src/Swarm/triangle_missions_decentralized.py
@@ -138,8 +138,16 @@ class TriangleMember(Mission):
         end_psi = self.formation_rot_target_angle
 
         delta_psi = remap(current_time, start_time, end_time, 0.0, end_psi)        
-        self.formation_rot_matrix = rotation_matrix(delta_psi, self.formation_rot_axis) * self.formation_rot_start_matrix
-
+        if(delta_psi > end_psi):
+            delta_psi = end_psi
+        # PROBLEM WITH THIS LINE
+        self.formation_rot_matrix = np.matmul(rotation_matrix(delta_psi, self.formation_rot_axis) , self.formation_rot_start_matrix)
+        if(self.uav_id == 3):
+            #print("formation start matrix: ")
+            #print(rotation_matrix(delta_psi, self.formation_rot_axis))
+            pass
+        #print("delta PSI:", delta_psi, "##");
+        #print(rotation_matrix(delta_psi, self.formation_rot_axis))
 
 
     def deserialize_data_from_buffer(self, buffer):
@@ -230,7 +238,7 @@ class TriangleMember(Mission):
         self.uav_vel_infos[self.uav_id].value = self.uav.get_current_velocity() # change for the Vector3 ds
         self.uav_vel_infos[self.uav_id].publish_data()
 
-        print("my id: ", self.uav_id)
+        #print("my id: ", self.uav_id)
 
 
         pos_sum = vec(0.0,0.0,0.0)
@@ -270,7 +278,7 @@ class TriangleMember(Mission):
 
     def get_targetpos_u_via_effective_point_cloud(self):
         effective_point_cloud = self.get_effective_point_cloud()
-
+        
         i = 0
         my_index = self.uav_id
         my_pos = self.uav_pos_infos[my_index].value
@@ -279,7 +287,15 @@ class TriangleMember(Mission):
             if(i != my_index):
                 other_pos = uav_pos_info.value
                 delta = other_pos - my_pos
-                target_delta = np.matmul(self.formation_rot_matrix, effective_point_cloud[i]) - np.matmul(self.formation_rot_matrix, effective_point_cloud[my_index])
+                
+                target_delta =  np.matmul(self.formation_rot_matrix,  effective_point_cloud[i]) -  np.matmul(self.formation_rot_matrix,  effective_point_cloud[my_index])
+                if(i == 3 and my_index == 0):
+                    print("mags-mags-mags-mags-mags-mags-mags")
+                    #print(np.linalg.norm(np.matmul(self.formation_rot_matrix,  effective_point_cloud[i])))
+                    print(self.formation_rot_matrix)
+                    
+                    #print(np.matmul(self.formation_rot_matrix,  effective_point_cloud[i]), np.matmul(self.formation_rot_matrix,  effective_point_cloud[my_index]))
+                    #print(np.linalg.norm(target_delta))
                 #target_delta = np.matmul(self.formation_rot_matrix,  target_delta) # check the multiplication. 
                 u -= target_delta - delta
             i+=1
@@ -302,7 +318,7 @@ class TriangleMember(Mission):
 
         for uav in range(uav_count):
             for point in range(uav_count):
-                cost_matrix[uav, point] = dist(uav_poses[uav], (np.matmul(self.formation_rot_matrix ,point_cloud[point]) + self.formation_pos))
+                cost_matrix[uav, point] = dist(uav_poses[uav], (np.matmul(self.formation_rot_matrix ,point_cloud[point])) + self.formation_pos)
                 
 
         row_ind, col_ind = linear_sum_assignment(cost_matrix)

src/swarm/src/Swarm/utility.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+import time
 
 class TwoWayDict(object):
     def __init__(self):
@@ -76,11 +77,16 @@ def remap(a, a0, a1, b0, b1):
 
 
 if __name__ == "__main__":
+    np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
 
-    mat0 = rotation_matrix(2*math.pi / 4.0)
-    vec0 = vec(1,0,0)
-    result = np.matmul(mat0, vec0)
-    result = np.matmul(mat0, result)
-    print(result)
+    print("STARTLOOP-STARTLOOP-STARTLOOP-STARTLOOP-STARTLOOP-STARTLOOP-STARTLOOP-STARTLOOP")
+    loopvec = rotation_matrix(0.0, "Z")
+    loopmat = rotation_matrix(2*math.pi * 0.5 * (1.0/100.0))
+    for i in range(100):
+        print(loopvec)
+        print("mag: ", np.linalg.norm(loopvec))
+        loopvec = np.matmul(loopmat, loopvec)
+        time.sleep(0.2)
+
     #print_mat(result)
     